0b859f876637b773a9923e9a5241c6e3b3da497f
J Scott Rumptz <jscott@rumptz.tech>
2021-03-05 16:15:25 -0600

    fixed connection

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,19 @@
+// import the sequelize constructor from library
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
 let sequelize;
 
+    // heroku connection
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    // create connection to our database, pass in your MySQL info
+    sequalize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
-  });
+    });
 }
+module.exports = sequalize;
