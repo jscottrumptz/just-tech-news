40c46ab2e267eaad4a6c11d74fee7464305bdcf1
J Scott Rumptz <jscott@rumptz.tech>
2021-03-05 16:04:51 -0600

    jawsdb part3

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,11 @@
-// import the sequelize constructor from library
-let Sequelize = require('sequelize');
+let sequelize;
 
-    // heroku connection
 if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-    // create connection to our database, pass in your MySQL info
-    sequalize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
-    });
+  });
 }
-module.exports = sequalize;
